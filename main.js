7688edcc4a5f1d53bb56c244fd0561d514e2f112
aleka <alekajobs@gmail.com>
2024-01-05 15:36:44 -0800

    added player 2, profiles, start

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 class Player {
-    constructor(playerType) {
+    constructor(playerType, tookTurn, name) {
+        this.name = name;
         this.playerType = playerType;
+        this.tookTurn = tookTurn;
     }
-    tookTurn = false;
     wins = 0;
 }
 
@@ -16,6 +17,16 @@ class GUI {
     startButton = document.getElementById('start');
     playAgainButton = document.getElementById('restart');
     board = document.getElementById('board');
+    displayProfiles() {
+        let player1 = document.getElementById('player1');
+        player1.innerText = this.player1.name;
+        let player2 = document.getElementById('player2');
+        player2.innerText = this.player2.name;
+        let player1Stats = document.getElementById('player1-stats');
+        player1Stats.innerText = this.player1.wins;
+        let player2Stats = document.getElementById('player2-stats');
+        player2Stats.innerText = this.player2.wins;
+    }
     createGrid() {
         for (let i = 0; i < 3; i++) {
             let row = document.createElement('tr');
@@ -24,14 +35,18 @@ class GUI {
                 cell.classList.add('cell');
                 cell.setAttribute('data-coordinate', `${i},${j}`);
                 cell.addEventListener('click', () => {
-                    cell.innerText = 'X';
                     this.myGame.playerChoose(i, j);
+                    cell.innerText = this.myGame.board[i][j];
+                    this.myGame.checkForWin();
                     console.log(this.myGame.board);
                 });
                 row.appendChild(cell);
             }
             board.appendChild(row);
         }
+    }
+    hideButton() {
+        this.startButton.style.display = 'none';
     }
 }
 
@@ -44,7 +59,7 @@ class Game {
              [3, 4, 5], 
              [6, 7, 8]];
     playerChoose(k, l) {
-        if (this.player1.playerType == 'X') {
+        if (this.player1.playerType == 'X' && this.player1.tookTurn == false) {
             for (let i = 0; i < this.board.length; i++) {
                 for (let j = 0; j < this.board.length; j++) {
                     if (i == k && j == l) {
@@ -74,10 +89,12 @@ class Game {
         if ((this.board[0][0] == this.board[0][1]) && (this.board[0][1] == this.board[0][2])) {
             if (this.board[0][2] == 'X') {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
@@ -85,10 +102,12 @@ class Game {
         if ((this.board[0][0] == this.board[1][0]) && (this.board[1][0] == this.board[2][0])) {
             if (this.board[2][0] == 'X') {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
@@ -96,10 +115,12 @@ class Game {
         else if ((this.board[1][0] == this.board[1][1]) && (this.board[1][1] == this.board[1][2])) {
             if (this.board[1][2] == "X") {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
@@ -107,10 +128,12 @@ class Game {
         else if ((this.board[0][2] == this.board[1][2]) && (this.board[1][2] == this.board[2][2])) {
             if (this.board[2][2] == 'X') {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
@@ -118,10 +141,12 @@ class Game {
         else if ((this.board[2][0] == this.board[2][1]) && (this.board[2][1] == this.board[2][2])) {
             if (this.board[2][2] == 'X') {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
@@ -129,10 +154,12 @@ class Game {
         else if ((this.board[0][0] == this.board[1][1]) && (this.board[1][1] == this.board[2][2])) {
             if (this.board[2][2] == 'X') {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
@@ -140,48 +167,78 @@ class Game {
         else if ((this.board[0][2] == this.board[1][1]) && (this.board[1][1] == this.board[2][0])) {
             if (this.board[2][0] == 'X') {
                 this.player1.wins++;
+                this.gameOver();
                 return true;
             }
             else {
                 this.player2.wins++;
+                this.gameOver();
                 return true;
             }
         }
         else return false;
     }
-    gameOver(player1, player2) {
+    gameOver() {
         if (this.player1.wins > this.player2.wins) {
             console.log("Player 1 wins!");
+            this.resetGame();
             return true;
         }
         else if (this.player1.wins < this.player2.wins) {
             console.log("Player 2 wins!");
+            this.resetGame();
             return true;
         }
         else if (this.player1.wins == this.player2.wins) {
             console.log("Tie!");
+            this.resetGame();
             return true;
         }
         else return false;
     }
+    resetGame() {
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                if (i == 0) this.board[i][j] = j;
+                else if (i == 1) this.board[i][j] = j + 3;
+                else this.board[i][j] = j + 6;
+            }
+        }
+    }
+}
+
+function hideButton(myGUI) {
+    myGUI.hideButton();
 }
 
 function main() {
-    let player1 = new Player('X');
-    let player2 = new Player('O');
-    player2.tookTurn = true;
+    let name1 = "PLACEHOLDER";
+    let name2 = "PLACEHOLDER";
+    let player1 = new Player('X', false, name1);
+    let player2 = new Player('O', true, name2);
     let myGame = new Game(player1, player2);
     let myGUI = new GUI(player1, player2, myGame);
 
-    myGUI.createGrid();
 
-    myGUI.startButton.addEventListener('click', () => {
-        if (myGame.checkForWin(player1, player2)) {
-            myGame.gameOver(player1, player2);
-        }
-        myGame.playerChoose(player1, player2);
+    myGUI.startButton.addEventListener('click', () => { 
+        let username1 = window.prompt("Please enter username for player 1.");
+        let username2 = window.prompt("Please enter username for player 2.");
+        player1.name = username1;
+        player2.name = username2;
+        myGUI.displayProfiles();
+        myGUI.createGrid();
+        hideButton(myGUI);
     })
+
+    /* if (myGame.checkForWin(player1, player2)) {
+            myGame.gameOver(player1, player2);
+        } */
     console.log(myGame.board);
+
+    myGUI.playAgainButton.addEventListener('click', () => {
+        player1.wins = 0;
+        player2.wins = 0;
+    })
 }
 
 main();
